7e4d87675ecdd1355de3c914405e526daec37c2b
Reydner Barros <reydner.alves@dcx.ufpb.br>
2026-05-11 10:09:44 -0300

    Parte 2
    na falta de coragem, vem a tentativa. Comecei (finalmente) a desenvolver melhor isso

---
+++ ProjetoEx2/src/main/java/Produto.java
@@ -0,0 +1,11 @@
+public class Produto { // o começo de tudo! decidi que iria fazer um mini gerenciador de produtos de supermercado
+    private String nome;
+    private boolean valor;
+    private String codigo;
+
+    public Produto(String nome, boolean valor, String codigo){
+        this.nome = nome;
+        this.valor = valor;
+        this.codigo = codigo;
+    }
+}
---
+++ ProjetoEx2/src/main/java/SistemaMain.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+// inicialmente, comecei usando na base do scanner mesmo, mas troquei pro JOptionPane
+
+public class SistemaMain {
+    static void main(String[] Args) {
+
+    }
+}
